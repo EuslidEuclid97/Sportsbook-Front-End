22a3e523b693326c8939b88c1881bc036d6efb8e
EuslidEuclid97 <76755131+EuslidEuclid97@users.noreply.github.com>
2022-09-25 21:20:52 -0500

    Update App.js
    Just a copy-paste commit cause Git is being difficult

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import NavBar from './NavBar';
+/*
+    File: App.js
+    Brief: The main component that calls the major components in the app
+    Programmer: Zack Andrews
+    Date created: 09/21/2022
+    Date of revision: 09/25/2022
+        Revision: Deleted some unneeded code
+        Author: Zack Andrews//
+*/
+
+import NavBar from './NavBar';//importing components (same for next two lines)
 import Home from './Home';
 import Account from './Account';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { useState } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';//importing features that create routes for different pages/components
 
-function App() {
+function App() {//JSX calls components
    
   return (
     <Router>
       <div className="App">
-        <NavBar/>
+        {/*<NavBar/>*/}
         <div className="content">
           <Routes>
             <Route exact path = "/" element = {<Home/>}></Route>
